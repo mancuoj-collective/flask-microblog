e10b49ae62ed8ccf2d1c7bc6f7df4e72ec0fa7fd
mancuoj <mancuoj@gmail.com>
2024-09-26 20:50:44 +0800

    fix: none type

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_FROM = os.environ.get("MAIL_FROM")
-    ADMINS = os.environ.get("ADMINS").split(",")
+    ADMINS = os.environ.get("ADMINS", "").split(",")
