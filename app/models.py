56a1f361b7f13407f8a82126c5614cb9a2cf06ca
mancuoj <mancuoj@gmail.com>
2024-09-26 20:59:21 +0800

    feat: follow

--- app/models.py
+++ app/models.py
@@ -9,6 +9,13 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db, login
 
+followers = sa.Table(
+    "followers",
+    db.metadata,
+    sa.Column("follower_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+    sa.Column("followed_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+)
+
 
 # UserMixin 提供了四个通用方法，is_authenticated, is_active, is_anonymous, get_id
 class User(UserMixin, db.Model):
@@ -23,6 +30,19 @@ class User(UserMixin, db.Model):
 
     # 反向关联，和 Post 建立双向通信
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+    # secondary 参数指定关联表，`.c` 用于访问关联表的列
+    following: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates="followers",
+    )
+    followers: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates="following",
+    )
 
     # __repr__ 方法用于在调试和日志中打印对象，便于调试
     def __repr__(self):
@@ -37,6 +57,54 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery()
+        )
+        return db.session.scalar(query)
+
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery()
+        )
+        return db.session.scalar(query)
+
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        query = (
+            sa.select(Post)
+            # 连接 Post 表和 Author 表
+            .join(Post.author.of_type(Author))
+            # 连接 Author 表和 Follower 表，isouter=True 表示左连接
+            # 左连接：将所有符合条件的记录都查询出来，即使 Follower 表中没有对应的记录
+            .join(Author.followers.of_type(Follower), isouter=True)
+            # 连接条件：Follower 表的 id 等于 self.id 或者 Author 表的 id 等于 self.id
+            .where(
+                sa.or_(
+                    Follower.id == self.id,
+                    Author.id == self.id,
+                )
+            )
+            # 分组：根据 Post 表的 id 分组去重
+            .group_by(Post)
+            # 排序：根据 Post 表的 timestamp 列降序排序
+            .order_by(Post.timestamp.desc())
+        )
+        return query
 
 
 # Flask-Login 需要从 session 中读取当前用户状态
